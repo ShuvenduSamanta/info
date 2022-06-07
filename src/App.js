79e0196171e2665114979b07bc055e78693fd2c2
ShuvenduSamanta <shuvendu247@gmail.com>
2022-06-07 22:24:48 +0530

    learn to git

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App"> 
+      <h1>Hello world!</h1>
+      <h2>This is Shuvendu</h2>
+      <h3>here this html tag has to be atleast one wrapper tag like div, table</h3>
+    </div>
+  );
+}
+
+export default App;
